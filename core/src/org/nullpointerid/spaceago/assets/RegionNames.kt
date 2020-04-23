2458f049d2fd41752cc1ed40617cf29251869785
Aleksandr <alrudo@ttu.ee>
2020-04-23 13:01:39 +0300

    Added assets.

--- core/src/org/nullpointerid/spaceago/assets/RegionNames.kt
+++ core/src/org/nullpointerid/spaceago/assets/RegionNames.kt
@@ -5,17 +5,13 @@ object RegionNames {
     // Region names for the assets in atlases. Each const value matches the associated picture name.
     const val MENU_BACKGROUND = "menu_background"
     const val GAMEPLAY_BACKGROUND = "gameplay_background"
-    const val MOVING_BACKGROUND = "moving_background"
-
-    const val MENU_PLAY_BUTTON_IDLE = "play_button_inactive"
-    const val MENU_PLAY_BUTTON_CLICKED = "play_button_active"
-    const val MENU_QUIT_BUTTON_IDLE = "exit_button_inactive"
-    const val MENU_QUIT_BUTTON_ACTIVE = "exit_button_active"
 
     const val BULLET = "bullet"
-    const val EXPLOSION = "explosion"
     const val PLAYER = "player"
     const val SIMPLE_ENEMY = "simple_enemy"
 
     const val GAME_OVER = "game_over"
+
+    const val MENU_FONT = "MenuFont"
+    const val MENU_FONT_YELLOW = "MenuFontYellow"
 }
